f3663b4f8562a2f6dfce4d8dcc5b04a40e0359b8
Michael Siers <msiers@csu.edu.au>
2016-07-27 19:14:17 +1000

    Now uses tabs instead of spaces. Indendation is also consistent now.

--- PyDPD.py
+++ PyDPD.py
@@ -1,12 +1,12 @@
 def getFileLines(filename):
-    """
-	Returns the lines from a file as a list.
+  """
+  Returns the lines from a file as a list.
 
-    :param filename: The file name of the file to get the lines of.
-	:returns: List[str] - A list of strings where each string represents a line 
-	in the file.
-    """
-    file = open(filename)
-    lines = file.readLines()
-    file.close()
-    return lines
+  :param filename: The file name of the file to get the lines of.
+  :returns: List[str] - A list of strings where each string represents a line 
+  in the file.
+  """
+  file = open(filename)
+  lines = file.readLines()
+  file.close()
+  return lines
